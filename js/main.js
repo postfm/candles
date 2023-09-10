e28d735f719050e8649f69cbeea40333066d2e16
postfm <postfm@yandex.ru>
2023-09-10 16:29:02 +0300

    Добавит оставшиеся секции

--- js/main.js
+++ js/main.js
@@ -30,3 +30,51 @@ menuBtn.addEventListener('click', () => {
   menuModal.classList.toggle('menu--open')
   console.log('click');
 })
+
+const swiperOne = new Swiper('.feedback__slider', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+const swiperTwo = new Swiper('.certificates__slider', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    360: {
+      slidesPerView: 1,
+    },
+  },
+});
+
+const accordion = document.querySelector('.accordion');
+const accordionTitles = accordion.querySelectorAll('.accordion__title')
+
+accordionTitles.forEach.call(accordionTitles, function (accordionTitle) {
+  accordionTitle.addEventListener('click', function () {
+
+    const currentText = accordionTitle.parentElement.querySelector('.accordion__text')
+
+    accordionTitle.classList.toggle('accordion__title--active')
+    currentText.classList.toggle('accordion__text--visible')
+
+    if (currentText.classList.contains('accordion__text--visible')) {
+      currentText.style.maxHeight = currentText.scrollHeight + 'px'
+    } else {
+      currentText.style.maxHeight = null
+    }
+
+  })
+})
